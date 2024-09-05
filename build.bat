068ca10df719589383c0966e6f78240b8fdbe7c9
Brian Oost <brianleon77@gmail.com>
2024-09-05 06:08:52 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
+if exist "publish" (
+    rmdir /s /q "publish"
+)
+if exist "build" (
+    rmdir /s /q "build"
+)
 dotnet publish -r win-x64 -c Release --self-contained -p:Platform="x64" -o ./publish
 rename publish\"Arctium WoW Launcher.exe" Arctium.exe
